24fb892750228dbbfe01aafb934533235259ab6e
olga-evdokimova <pavedom13@gmail.com>
2023-10-23 09:25:46 +0300

    about

--- app/components/Products/About/About.tsx
+++ app/components/Products/About/About.tsx
@@ -4,16 +4,24 @@ import "./About.scss";
 export default function About() {
   return (
     <section className="about">
-      <Link href="#">About us</Link>
-      <h3 className="about__title">
-        We create digital products and services for ambitious startups and
-        companies
-      </h3>
-      <p className="about__text">
-        We will analyze the business and conduct research to develop and
-        maintain an effective product. If necessary, we will update the identity
-        or make branding from scratch.
-          </p>
+      <div className="about__content">
+        <Link href="#">About the company</Link>
+        <h3 className="about__title">
+          Providing cutting-edge solutions for mobile communications worldwide.
+        </h3>
+        <p className="about__text">
+          With an international team of seasoned professionals, we specialize in
+          creating highly adaptable and robust products for seamless data
+          provisioning, efficient data and voice sharing, innovative data
+          gifting, as well as streamlined data and voice distribution.
+        </p>
+        <p className="about__text">
+          Backed by unmatched technical assistance, our 5-star user interfaces,
+          available on both mobile and browser platforms, guarantee a
+          user-friendly experience, enabling our clients to effortlessly stay
+          connected and communicate seamlessly worldwide.
+        </p>
+      </div>
     </section>
   );
 }
