37a4e6d3f08353dfd2a3e2c5a0700ee7ba568084
olga-evdokimova <pavedom13@gmail.com>
2023-10-27 15:34:05 +0300

    platform__svg

--- app/components/Platform/Platform.tsx
+++ app/components/Platform/Platform.tsx
@@ -4,9 +4,110 @@ import Image from "next/image";
 export default function Platform() {
   return (
     <section className="platform container">
-      <div className="platform__img">
-        <Image src={"/platform-img.png"} alt="logo" fill></Image>
-      </div>
+      <svg className="platform__svg"
+        width="2485"
+        height="2080"
+        viewBox="0 0 2485 2080"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g opacity="0.5">
+          <g filter="url(#filter0_f_650_15)">
+            <ellipse
+              cx="441.5"
+              cy="414"
+              rx="441.5"
+              ry="414"
+              transform="matrix(1 0 0 -1 1202 1228)"
+              fill="#D6C7B4"
+            />
+          </g>
+          <g filter="url(#filter1_f_650_15)">
+            <circle
+              cx="528.5"
+              cy="528.5"
+              r="528.5"
+              transform="matrix(1 0 0 -1 400 1680)"
+              fill="#309BD8"
+            />
+          </g>
+          <g filter="url(#filter2_f_650_15)">
+            <circle
+              cx="267"
+              cy="267"
+              r="267"
+              transform="matrix(1 0 0 -1 1110 1665)"
+              fill="#D6C7B4"
+            />
+          </g>
+        </g>
+        <defs>
+          <filter
+            id="filter0_f_650_15"
+            x="802"
+            y="0"
+            width="1683"
+            height="1628"
+            filterUnits="userSpaceOnUse"
+            color-interpolation-filters="sRGB"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            />
+            <feGaussianBlur
+              stdDeviation="200"
+              result="effect1_foregroundBlur_650_15"
+            />
+          </filter>
+          <filter
+            id="filter1_f_650_15"
+            x="0"
+            y="223"
+            width="1857"
+            height="1857"
+            filterUnits="userSpaceOnUse"
+            color-interpolation-filters="sRGB"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            />
+            <feGaussianBlur
+              stdDeviation="200"
+              result="effect1_foregroundBlur_650_15"
+            />
+          </filter>
+          <filter
+            id="filter2_f_650_15"
+            x="710"
+            y="731"
+            width="1334"
+            height="1334"
+            filterUnits="userSpaceOnUse"
+            color-interpolation-filters="sRGB"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            />
+            <feGaussianBlur
+              stdDeviation="200"
+              result="effect1_foregroundBlur_650_15"
+            />
+          </filter>
+        </defs>
+      </svg>
+
       <div className="platform__wrap">
         <div className="platform__card">
           <Title>40%</Title>
