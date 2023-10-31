98571f8768597ea1e4bf12d73d372d1c9b17a7f5
olga-evdokimova <pavedom13@gmail.com>
2023-10-31 20:13:50 +0300

    ModalCases

--- app/components/ModalCases/ModalCases.tsx
+++ app/components/ModalCases/ModalCases.tsx
@@ -10,9 +10,29 @@ export default function Modal({ onCloseModal }: ModalProps) {
   return (
     <div className="modal">
       <button className="modal__close" onClick={onCloseModal}>
-        &#10006;
+        <svg
+          className="modal__close-svg"
+          width="42"
+          height="42"
+          viewBox="0 0 42 42"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M41 1L21 21M1 41L21 21M21 21L1 1L41 41"
+            stroke="white"
+            stroke-width="2"
+            stroke-linecap="round"
+          />
+        </svg>
       </button>
-      <Image className="modal__img" src={"/nigeria-modal.png"} alt="" width={955} height={769}></Image>
+      <Image
+        className="modal__img"
+        src={"/nigeria-modal.png"}
+        alt=""
+        width={955}
+        height={769}
+      ></Image>
       <div className="modal__content">
         <h6>
           <Image src={"/nigeria.png"} alt="" width={41} height={22}></Image>
@@ -34,10 +54,7 @@ export default function Modal({ onCloseModal }: ModalProps) {
             />
           </svg>
         </Link>
-        <p>
-          Advantages of working with us Advantages of working with usAdvantages
-          of working
-        </p>
+
         <Link href={""}>
           Data distribution
           <svg
@@ -54,10 +71,6 @@ export default function Modal({ onCloseModal }: ModalProps) {
             />
           </svg>
         </Link>
-        <p>
-          Advantages of working with us Advantages of working with usAdvantages
-          of working
-        </p>
       </div>
     </div>
   );
