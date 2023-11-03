db6f7ba7e7319458127565f6ab4e25608e308291
olga-evdokimova <pavedom13@gmail.com>
2023-11-03 11:35:24 +0300

    cases-map

--- app/components/Cases/Cases.tsx
+++ app/components/Cases/Cases.tsx
@@ -2,14 +2,13 @@
 import Title from "../Title/Title";
 import "./Cases.scss";
 import Partners from "../Partners/Partners";
-import CasesSvgMap from "../CasesSvgMap/CasesSvgMap";
 import Nigeria from "../Nigeria/Nigeria";
 import Congo from "../Congo/Congo";
 import Kenya from "../Kenya/Kenya";
 export default function Cases() {
   return (
     <section className="cases container">
-      <CasesSvgMap />
+    <div className="cases__map"></div>
       <Nigeria />
       <Congo />
       <Kenya />
