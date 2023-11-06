a13886a08a281e4981d195ed3651e428a6f36562
olga-evdokimova <pavedom13@gmail.com>
2023-11-06 22:41:17 +0300

    Burger

--- app/components/Nav/Burger.jsx
+++ app/components/Nav/Burger.jsx
@@ -1,58 +1,6 @@
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
 import RightNav from "./RightNav";
 
-const StyledBurger = styled.div`
-  width: 50px;
-  height: 50px;
-  position: absolute;
-  top: 40px;
-  right: 40px;
-  z-index: 150;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  cursor: pointer;
-  @media (max-width: 767px) {
-    top: 15px;
-    right: 20px;
-  }
-  span {
-    width: 45px;
-    height: 2px;
-    background-color: ${({ open }) =>
-      open ? "var(--color-text-black)" : "var(--color-text-white)"};
-    border-radius: 10px;
-    transform-origin: 1px;
-    transition: all 0.3s linear;
-    margin-bottom: 10px;
-    margin-left: auto;
-    &:last-child {
-      width: 40px;
-      margin-bottom: 0;
-    }
-    &:first-child {
-      width: 50px;
-    }
-    &:nth-child(1) {
-      transform: ${({ open }) =>
-        open ? "rotate(45deg) translate(0px, -7px)" : "rotate(0) translate(0)"};
-    }
-
-    &:nth-child(2) {
-      //   transform: ${({ open }) =>
-        open ? "translateX(100%)" : "translateX(0)"};
-      opacity: ${({ open }) => (open ? 0 : 1)};
-    }
-
-    &:nth-child(3) {
-      transform: ${({ open }) =>
-        open ? "rotate(-45deg) translate(0px, 7px)" : "rotate(0) translate(0)"};
-          width: ${({ open }) => (open ? "50px" : "40px")};
-    }
-    
-  }
-`;
 
 export default function Burger() {
   const [open, setOpen] = useState(false);
@@ -76,11 +24,11 @@ export default function Burger() {
 
   return (
     <>
-      <StyledBurger open={open} onClick={() => setOpen(!open)}>
+      <div className="burger" open={open} onClick={() => setOpen(!open)}>
         <span />
         <span />
         <span />
-      </StyledBurger>
+      </div>
       <RightNav open={open} />
     </>
   );
--- app/components/Nav/RightNav.jsx
+++ app/components/Nav/RightNav.jsx
@@ -1,168 +1,108 @@
-import { StyleSheetManager } from "styled-components";
-import isPropValid from "@emotion/is-prop-valid";
 import Link from "next/link";
-import React, { useState } from "react";
-import styled from "styled-components";
-
-const Ul = styled.ul`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-end;
-  background-color: var(--color-text-white);
-  position: absolute;
-  transform: ${({ open }) => (open ? "translateX(0)" : "translateX(100%)")};
-  top: 0;
-  right: 0;
-  height: 100vh;
-  min-width: 140px;
-  transition: transform 0.3s ease-in-out;
-  padding: 80px 40px 0;
-  z-index: 100;
-  overflow: auto;
-  @media (max-width: 767px) {
-    padding: 60px 20px 0 20px;
-  }
-  .nav-menu {
-    font-weight: 700;
-    font-size: 20px;
-    padding: 0 0px 5px;
-    margin: 50px 10px 0;
-    cursor: pointer;
-    &__list {
-      padding: 30px 60px 20px 60px;
-      transform: translateX(100%);
-      animation: slideIn 0.5s ease-in-out forwards;
-      @media (max-width: 767px) {
-        padding: 20px 0 0px;
-      }
-      @media (max-width: 375px) {
-        width: 100vw;
-      }
-      &-item {
-        color: var(--color-text-black);
-        text-align: right;
-
-        a {
-          display: block;
-          font-weight: 400;
-          font-size: 20px;
-          padding: 0 0 30px 0;
-        }
-        span {
-          display: block;
-          font-style: italic;
-          opacity: 0.3;
-        }
-      }
-    }
-  }
-  @keyframes slideIn {
-    0% {
-      transform: translateX(100%);
-    }
-    100% {
-      transform: translateX(0);
-    }
-  }
-`;
-
-const Li = styled.li`
-  color: ${(props) =>
-    props.clicked ? "var(--color-accent)" : "var(--color-text-black)"};
-  border-bottom: ${(props) =>
-    props.clicked
-      ? "2px solid transparent"
-      : "2px solid var(--color-text-black)"};
-`;
+import React, { useState} from "react";
+import "./style.scss";
 
 export default function RightNav({ open }) {
   const [showListB2C, setShowListB2C] = useState(false);
   const [showListB2B, setShowListB2B] = useState(false);
-  const [isB2CClicked, setIsB2CClicked] = useState(false);
-  const [isB2BClicked, setIsB2BClicked] = useState(false);
+  const [buttonColorB2B, setButtonColorB2B] = useState("black");
+  const [buttonColorB2C, setButtonColorB2C] = useState("black");
+  const [BorderB2C, setBorderB2C] = useState(true);
+  const [BorderB2B, setBorderB2B] = useState(true);
+  
 
   const handleItemClickB2C = () => {
     setShowListB2C(!showListB2C);
-    setIsB2CClicked(!isB2CClicked);
+    setButtonColorB2C((prevColor) =>
+      prevColor === "black" ? "#e0933e" : "black"
+    );
+    setBorderB2C((prevBorder) => !prevBorder);
   };
 
   const handleItemClickB2B = () => {
     setShowListB2B(!showListB2B);
-    setIsB2BClicked(!isB2BClicked);
+    setButtonColorB2B((prevColor) =>
+      prevColor === "black" ? "#e0933e" : "black"
+    );
+    setBorderB2B((prevBorder) => !prevBorder);
   };
 
   return (
-    <StyleSheetManager shouldForwardProp={(prop) => isPropValid(prop)}>
-      <Ul open={open}>
-        <Li
-          className="nav-menu"
-          clicked={isB2CClicked}
-          onClick={handleItemClickB2C}
-        >
-          B2C
-        </Li>
-        {showListB2C && (
-          <ul className="nav-menu__list">
-            <li className="nav-menu__list-item">
-              <Link href={"/products/b2c-data-voice-sharing"}>
-                Data & Voice Sharing<span>Family</span>
-              </Link>
-            </li>
-            <li className="nav-menu__list-item">
-              <Link href={"/products/b2c-data-voice-cost-sharing"}>
-                Data & Voice Cost Sharing<span>Friends and social buying</span>
-              </Link>
-            </li>
-            <li className="nav-menu__list-item">
-              <Link href={"/products/b2c-social-network-bundles"}>
-                Social Network Bundles<span>Individuals and social buying</span>
-              </Link>
-            </li>
-            <li className="nav-menu__list-item">
-              <Link href={"/products/b2c-airtime-credit"}>
-                Airtime Credit<span>Individuals</span>
-              </Link>
-            </li>
-            <li className="nav-menu__list-item">
-              <Link href={"/products/b2c-content-services"}>
-                Content Services<span>Individuals</span>
-              </Link>
-            </li>
-            {/* <li className="nav-menu__list-item">
+    <ul open={open} className="ul">
+      <li
+        className="nav-menu"
+        style={{
+          color: buttonColorB2C,
+          borderBottom: BorderB2C ? "2px solid black" : "none",
+        }}
+        onClick={handleItemClickB2C}
+      >
+        B2C
+      </li>
+      {showListB2C && (
+        <ul className="nav-menu__list">
+          <li className="nav-menu__list-item">
+            <Link href={"/products/b2c-data-voice-sharing"}>
+              Data & Voice Sharing<span>Family</span>
+            </Link>
+          </li>
+          <li className="nav-menu__list-item">
+            <Link href={"/products/b2c-data-voice-cost-sharing"}>
+              Data & Voice Cost Sharing<span>Friends and social buying</span>
+            </Link>
+          </li>
+          <li className="nav-menu__list-item">
+            <Link href={"/products/b2c-social-network-bundles"}>
+              Social Network Bundles<span>Individuals and social buying</span>
+            </Link>
+          </li>
+          <li className="nav-menu__list-item">
+            <Link href={"/products/b2c-airtime-credit"}>
+              Airtime Credit<span>Individuals</span>
+            </Link>
+          </li>
+          <li className="nav-menu__list-item">
+            <Link href={"/products/b2c-content-services"}>
+              Content Services<span>Individuals</span>
+            </Link>
+          </li>
+          {/* <li className="nav-menu__list-item">
               <Link href={"/products/b2c-cvm-campaigns-management"}>
                 CVM campaigns management
                 <span>HV customers</span>
               </Link>
             </li> */}
-          </ul>
-        )}
-        <Li
-          className="nav-menu"
-          clicked={isB2BClicked}
-          onClick={handleItemClickB2B}
-        >
-          B2B
-        </Li>
-        {showListB2B && (
-          <ul className="nav-menu__list">
-            <li className="nav-menu__list-item">
-              <Link href={""}>
-                Data & Voice Sharing<span>Family</span>
-              </Link>
-            </li>
-            <li className="nav-menu__list-item">
-              <Link href={""}>
-                Data & Voice Cost Sharing<span>Friends and social buying</span>
-              </Link>
-            </li>
-            <li className="nav-menu__list-item">
-              <Link href={""}>
-                Social Network Bundles<span>Individuals and social buying</span>
-              </Link>
-            </li>
-          </ul>
-        )}
-      </Ul>
-    </StyleSheetManager>
+        </ul>
+      )}
+      <li
+        className="nav-menu"
+        style={{
+          color: buttonColorB2B,
+          borderBottom: BorderB2B ? "2px solid black" : "none",
+        }}
+        onClick={handleItemClickB2B}
+      >
+        B2B
+      </li>
+      {showListB2B && (
+        <ul className="nav-menu__list">
+          <li className="nav-menu__list-item">
+            <Link href={""}>
+              Data & Voice Sharing<span>Family</span>
+            </Link>
+          </li>
+          <li className="nav-menu__list-item">
+            <Link href={""}>
+              Data & Voice Cost Sharing<span>Friends and social buying</span>
+            </Link>
+          </li>
+          <li className="nav-menu__list-item">
+            <Link href={""}>
+              Social Network Bundles<span>Individuals and social buying</span>
+            </Link>
+          </li>
+        </ul>
+      )}
+    </ul>
   );
 }
