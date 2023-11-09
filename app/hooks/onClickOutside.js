39b969b893af76c0735bdc1a1dfcd116aa00be51
olga-evdokimova <pavedom13@gmail.com>
2023-11-09 12:54:15 +0300

    delete context & hooks

--- app/hooks/onClickOutside.js
+++ app/hooks/onClickOutside.js
@@ -1,20 +1,20 @@
 
-import { useEffect } from "react";
+// import { useEffect } from "react";
 
-const useOnClickOutside = (ref, handler) => {
-  useEffect(() => {
-    const listener = (event) => {
-      if (!ref.current || ref.current.contains(event.target)) {
-        return;
-      }
-      handler(event);
-    };
-    // document.addEventListener("mousedown", listener);
-    document.removeEventListener("mousedown", listener);
-    return () => {
-      document.removeEventListener("mousedown", listener);
-    };
-  }, [ref, handler]);
-};
+// const useOnClickOutside = (ref, handler) => {
+//   useEffect(() => {
+//     const listener = (event) => {
+//       if (!ref.current || ref.current.contains(event.target)) {
+//         return;
+//       }
+//       handler(event);
+//     };
+//     // document.addEventListener("mousedown", listener);
+//     document.removeEventListener("mousedown", listener);
+//     return () => {
+//       document.removeEventListener("mousedown", listener);
+//     };
+//   }, [ref, handler]);
+// };
 
-export default useOnClickOutside;
+// export default useOnClickOutside;
