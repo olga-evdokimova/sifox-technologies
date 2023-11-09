0aacbdabde5c3f3d15359a65d231b3ec660f56cb
olga-evdokimova <pavedom13@gmail.com>
2023-11-09 18:13:35 +0300

    privacy-policy Typographer

--- app/privacy-policy/page.tsx
+++ app/privacy-policy/page.tsx
@@ -25,15 +25,15 @@ export default function PrivacyPolicy() {
       <Title> Personal data conditions</Title>
       <AcccentTitle>User Agreement:</AcccentTitle>
       <p className="privacy-policy__text">
-        Welcome to Sifox Technologies' website. By using our services and
-        providing your personal information, you agree to the terms and
-        conditions outlined below:
+        Welcome to&nbsp;Sifox Technologies&rsquo; website. By&nbsp;using our
+        services and providing your personal information, you agree to&nbsp;the
+        terms and conditions outlined below:
       </p>
       <AcccentTitle>1. Information Collection: </AcccentTitle>
       <p className="privacy-policy__text">
         Sifox Technologies may collect personal data, including but not limited
-        to your name, telephone number, and email address, when you choose to
-        send us your questions or feedback via the website.
+        to&nbsp;your name, telephone number, and email address, when you choose
+        to send&nbsp;us your questions or&nbsp;feedback via the website.
       </p>
       <AcccentTitle>2. Data Usage:</AcccentTitle>
       <p className="privacy-policy__text">
@@ -45,45 +45,48 @@ export default function PrivacyPolicy() {
       </p>
       <AcccentTitle>3. Data Protection:</AcccentTitle>
       <p className="privacy-policy__text">
-        Sifox Technologies is committed to protecting your privacy. We employ
-        industry-standard security measures to prevent unauthorized access,
-        disclosure, or alteration of your personal information.
+        Your personal information will be&nbsp;used solely for the purpose of
+        addressing your inquiries or&nbsp;feedback. Sifox Technologies may also
+        use your information for internal record-keeping, improvement
+        of&nbsp;our products and services, and occasional promotional emails
+        if&nbsp;you have opted to&nbsp;receive them.
       </p>
       <AcccentTitle>4. Data Sharing:</AcccentTitle>
       <p className="privacy-policy__text">
-        Sifox Technologies does not sell, trade, or otherwise transfer your
-        personal information to outside parties unless required by law or as
-        necessary to fulfill your requests.
+        Sifox Technologies does not sell, trade, or&nbsp;otherwise transfer your
+        personal information to&nbsp;outside parties unless required by&nbsp;law
+        or&nbsp;as necessary to&nbsp;fulfill your requests.
       </p>
       <AcccentTitle>5. Cookies and Tracking:</AcccentTitle>
       <p className="privacy-policy__text">
-        By using our website, you agree to the use of cookies and other tracking
-        technologies to enhance your browsing experience and gather information
-        about website traffic and usage patterns. You can adjust your browser
-        settings to refuse cookies, although this may affect some website
-        functionalities.
+        By&nbsp;using our website, you agree to&nbsp;the use of&nbsp;cookies and
+        other tracking technologies to&nbsp;enhance your browsing experience and
+        gather information about website traffic and usage patterns. You can
+        adjust your browser settings to&nbsp;refuse cookies, although this may
+        affect some website functionalities.
       </p>
       <AcccentTitle>6. Age Restrictions:</AcccentTitle>
       <p className="privacy-policy__text">
-        Sifox Technologies' website is intended for use by individuals who are
-        at least 18 years old. If you are under 18, you may use the website
-        under the supervision of a parent or legal guardian.
+        Sifox Technologies&rsquo; website is&nbsp;intended for use
+        by&nbsp;individuals who are at&nbsp;least 18&nbsp;years old. If&nbsp;you
+        are under&nbsp;18, you may use the website under the supervision
+        of&nbsp;a&nbsp;parent or&nbsp;legal guardian.
       </p>
       <AcccentTitle>7. Changes to Agreement:</AcccentTitle>
       <p className="privacy-policy__text">
-        Sifox Technologies reserves the right to modify or update this User
-        Agreement at any time. Users are encouraged to review this agreement
-        periodically for any changes.
+        Sifox Technologies reserves the right to&nbsp;modify or&nbsp;update this
+        User Agreement at&nbsp;any time. Users are encouraged to&nbsp;review
+        this agreement periodically for any changes.
       </p>
       <p className="privacy-policy__text">
-        By using the Sifox Technologies website, you agree to the terms and
-        conditions stated above. If you do not agree with any part of this
-        agreement, please refrain from using our website or providing any
-        personal information.
+        By&nbsp;using the Sifox Technologies website, you agree to&nbsp;the
+        terms and conditions stated above. If&nbsp;you do&nbsp;not agree with
+        any part of&nbsp;this agreement, please refrain from using our website
+        or&nbsp;providing any personal information.
       </p>
       <p className="privacy-policy__text">
-        For any further information or inquiries regarding this agreement,
-        please contact us at (insert contact information).
+        For any further information or&nbsp;inquiries regarding this agreement,
+        please contact&nbsp;us at&nbsp;[insert contact information].
       </p>
       <p className="privacy-policy__text">
         Thank you for visiting Sifox Technologies.
