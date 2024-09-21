0d53394c153f9c131bd6ea56c752ee7ea294afe7
olga-evdokimova <pavedom13@gmail.com>
2024-09-21 20:55:05 +0300

    *

--- app/components/About/About.tsx
+++ app/components/About/About.tsx
@@ -10,49 +10,7 @@ export default function About() {
   return (
     <section className="about" id="about">
       <div className="about__content">
-        {/* <div className="about__ellipses"></div> */}
-        <svg
-          className="about__ellipses"
-          width="1557"
-          height="1557"
-          viewBox="0 0 1557 1557"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <g filter="url(#filter0_f_310_396)">
-            <circle
-              cx="378.5"
-              cy="378.5"
-              r="378.5"
-              transform="matrix(1 0 0 -1 400 1157)"
-              fill="#309BD8"
-            />
-          </g>
-          <defs>
-            <filter
-              id="filter0_f_310_396"
-              x="0"
-              y="0"
-              width="1557"
-              height="1557"
-              filterUnits="userSpaceOnUse"
-              color-interpolation-filters="sRGB"
-            >
-              <feFlood flood-opacity="0" result="BackgroundImageFix" />
-              <feBlend
-                mode="normal"
-                in="SourceGraphic"
-                in2="BackgroundImageFix"
-                result="shape"
-              />
-              <feGaussianBlur
-                stdDeviation="200"
-                result="effect1_foregroundBlur_310_396"
-              />
-            </filter>
-          </defs>
-        </svg>
-
+        <div className="about__ellipses"></div>
         <Link href={"/"} className="header__logo">
           <Image
             src={"/logo.svg"}
